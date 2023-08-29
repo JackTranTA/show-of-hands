64ea247460c5d49c0bb09822720d2d4f6531152c
Difan <dfhou29@gmail.com>
2023-08-28 20:27:21 -0700

    update server router config

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const adminRoutes = require('./routes/admin');
+const pollsRoute = require('./routes/polls');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -40,6 +41,7 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/admins', adminRoutes);
+app.use('/polls', pollsRoute);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
