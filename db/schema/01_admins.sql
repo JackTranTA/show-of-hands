62e3af1b30dd5bf8ce50ea840dc84e7b49b75af9
Difan <dfhou29@gmail.com>
2023-08-29 16:59:16 -0700

    update schema and seeds

--- db/schema/01_admins.sql
+++ db/schema/01_admins.sql
@@ -3,6 +3,5 @@ CREATE TABLE admins
 (
   id          SERIAL PRIMARY KEY NOT NULL,
   name        VARCHAR(255)       NOT NULL,
-  email       VARCHAR(320)       NOT NULL,
-  send_result BOOLEAN DEFAULT FALSE
+  email       VARCHAR(320)       NOT NULL
 );
