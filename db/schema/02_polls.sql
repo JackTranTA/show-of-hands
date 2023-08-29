c3fc74bf3a3412799642ba346de47468af84f38a
Difan <dfhou29@gmail.com>
2023-08-29 02:26:19 -0700

    update table schema

--- db/schema/02_polls.sql
+++ db/schema/02_polls.sql
@@ -1,11 +1,13 @@
 DROP TABLE IF EXISTS polls CASCADE;
 CREATE TABLE polls
 (
-  id          SERIAL PRIMARY KEY            NOT NULL,
-  creator_id  INTEGER REFERENCES users (id) NOT NULL,
-  title       VARCHAR(255)                  NOT NULL,
-  created_at  TIMESTAMP                     NOT NULL,
-  expired_at  TIMESTAMP,
-  visit_count INTEGER DEFAULT 0,
-  allow_anonymous BOOLEAN DEFAULT FALSE
+  id               SERIAL PRIMARY KEY            NOT NULL,
+  admin_id       INTEGER REFERENCES admins (id) NOT NULL,
+  title            VARCHAR(255)                  NOT NULL,
+  created_at       TIMESTAMP                     NOT NULL,
+  expired_at       TIMESTAMP,
+  visit_count      INTEGER DEFAULT 0,
+  allow_anonymous  BOOLEAN DEFAULT FALSE,
+  admin_identifier VARCHAR(50)                   NOT NULL,
+  voter_identifier VARCHAR(50)                   NOT NULL
 );
--- db/schema/06_poll_selections.sql
+++ db/schema/06_poll_selections.sql
@@ -3,7 +3,7 @@ CREATE TABLE poll_selections
 (
   id             SERIAL PRIMARY KEY                                     NOT NULL,
   poll_option_id INTEGER REFERENCES poll_options (id) ON DELETE CASCADE NOT NULL,
-  user_id        INTEGER REFERENCES users (id) ON DELETE CASCADE        NOT NULL,
+  voter_id        INTEGER REFERENCES voters (id) ON DELETE CASCADE        NOT NULL,
   poll_id        INTEGER REFERENCES polls (id) ON DELETE CASCADE        NOT NULL,
   score          INTEGER DEFAULT 0
 );
