3bf4ae9641469382e7838d7e1f33fcd7c36f80bb
Difan <dfhou29@gmail.com>
2023-08-29 02:26:45 -0700

    add seeds

--- db/seeds/01_users.sql
+++
@@ -1,7 +0,0 @@
-truncate table users CASCADE;
-ALTER SEQUENCE users_id_seq RESTART WITH 1;
-INSERT INTO users (name, email, is_admin)
-VALUES ('admin', 'admin@email.com', true),
-       ('voter1', 'voter1@email.com', false),
-       ('voter2', 'voter2@email.com', false),
-       ('voter3', 'voter3@email.com', false);
--- db/seeds/03_poll_options_seeds.sql
+++ db/seeds/03_poll_options_seeds.sql
@@ -1,6 +1,6 @@
 truncate table poll_options CASCADE;
 ALTER SEQUENCE poll_options_id_seq RESTART WITH 1;
-INSERT INTO poll_options (poll_id, option_title, description)
+INSERT INTO poll_options (poll_id, option_title, option_description)
 VALUES (1, 'option 1', 'desc for option 1'),
        (1, 'option 2', 'desc for option 2'),
        (1, 'option 3', 'desc for option 3');
