31b63c6c70a2c97c8579c676d348cfcde2e901df
Jack Tran <bluestar113311@gmail.com>
2023-08-30 18:35:52 -0700

    Fix db reset error

--- db/schema/06_poll_selections.sql
+++ db/schema/06_poll_selections.sql
@@ -3,6 +3,6 @@ CREATE TABLE poll_selections
 (
   id             SERIAL PRIMARY KEY                                     NOT NULL,
   poll_option_id INTEGER REFERENCES poll_options (id) ON DELETE CASCADE NOT NULL,
-  voter_id        INTEGER REFERENCES voters (id) ON DELETE CASCADE        NOT NULL,
+  voter_id       INTEGER REFERENCES voters (id) ON DELETE CASCADE       NOT NULL,
   score          INTEGER DEFAULT 0
 );
