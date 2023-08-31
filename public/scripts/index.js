ea2336220b75b7d2fe70ad9cb4e8d5f3f3187410
Difan <dfhou29@gmail.com>
2023-08-30 20:55:59 -0700

    edit script

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,22 +6,6 @@ $(() => {
       event.preventDefault();
       event.stopPropagation();
     }
-    // } else {
-    //   event.preventDefault();
-    //
-    //   const formData = $form.serialize();
-    //   $.ajax({
-    //     method: 'POST',
-    //     url: '/admins/',
-    //     data: formData,
-    //     success: (res) => {
-    //       console.log(res);
-    //     },
-    //     error: (error) => {
-    //       console.log("error", error);
-    //     }
-    //   })
-    //}
     $form.addClass('was-validated');
   });
 });
--- public/scripts/poll.js
+++ public/scripts/poll.js
@@ -4,22 +4,27 @@ function getCurrentDateAndTime() {
 }
 
 const addCandidate = function(count) {
-  let $candidate = `<article>
-          <div class="form-floating">
-            <input type="text" placeholder="Candidate title" name="candidate-title-${count}" class="form-control" required>
-            <label for="candidate-title-${count}">Candidate title</label>
-            <div class="valid-feedback">
-              Looks good!
-            </div>
-            <div class="invalid-feedback">
-              Please enter the candidate title!
-            </div>
-          </div>
-          <div class="form-floating">
-            <input type="text" placeholder="Candidate description" name="candidate-description-${count}" class="form-control">
-            <label for="candidate-description-${count}">Candidate description</label>
-          </div>
-        </article>`
+
+  let $candidate = `<div class="row mb-3">
+      <div class="form-group col-6 mx-auto">
+        <label for="candidate-title-0">Candidate title</label>
+        <input type="text" name="candidate-title-0" class="form-control form-control-lg" required>
+        <div class="valid-feedback">
+          Looks good!
+        </div>
+        <div class="invalid-feedback">
+          Please enter the candidate title!
+        </div>
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div class="form-group col-6 mx-auto">
+        <label for="candidate-description-0">Candidate description</label>
+        <input type="text" name="candidate-description-0" class="form-control form-control-lg">
+      </div>
+    </div>`;
+
   return $candidate;
 }
 
@@ -29,8 +34,19 @@ $(document).ready(function() {
     "min" : getCurrentDateAndTime()
   });
 
-  $("#add-candidate").click(function(){
+  $("#add-candidate").on('click', function(){
     count++;
-    $("article").last().append(addCandidate(count));
-  })
+    $(".add-candidate-container").before(addCandidate(count));
+  });
+
+  // form validation
+
+  const $form = $('.needs-validation');
+  $form.on('submit', function(event) {
+    if (!this.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    $form.addClass('was-validated');
+  });
 });
