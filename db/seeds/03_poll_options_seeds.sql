62bdcd6443100393539581180c1e2291b86420c2
Jack Tran <bluestar113311@gmail.com>
2023-08-31 23:20:43 -0700

    Update seed files

--- db/seeds/03_poll_options_seeds.sql
+++ db/seeds/03_poll_options_seeds.sql
@@ -2,5 +2,7 @@ truncate table poll_options CASCADE;
 ALTER SEQUENCE poll_options_id_seq RESTART WITH 1;
 INSERT INTO poll_options (poll_id, title, description)
 VALUES (1, 'option 1', 'desc for option 1'),
-       (1, 'option 2', 'desc for option 2'),
-       (1, 'option 3', 'desc for option 3');
+       (1, 'option 2', ''),
+       (1, 'option 3', 'desc for option 3'),
+       (1, 'option 4', 'desc for option 4'),
+       (1, 'option 5', 'desc for option 5');
--- db/seeds/04_voters_seeds.sql
+++ db/seeds/04_voters_seeds.sql
@@ -1,6 +1,6 @@
 truncate table voters CASCADE;
 ALTER SEQUENCE voters_id_seq RESTART WITH 1;
-INSERT INTO voters (poll_id, name)
-VALUES (1, 'voter1'),
-       (1, 'voter2'),
-       (1, 'voter3');
+INSERT INTO voters (poll_id, name, comment)
+VALUES (1, 'voter1', 'This poll is good'),
+       (1, 'voter2', ''),
+       (1, 'voter3', '');
