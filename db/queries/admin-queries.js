64a96ad5899d56ec02b4f6eb6b2c3f318416337d
Difan <dfhou29@gmail.com>
2023-09-01 03:41:16 -0700

    update query

--- db/queries/admin-queries.js
+++ db/queries/admin-queries.js
@@ -55,14 +55,14 @@ const getVotesDetailById = (id) => {
     SELECT voters.name                          AS voter_name,
            voters.id                            AS voter_id,
            voters.poll_id,
-           comments,
+           comment,
            ARRAY_AGG(poll_options.title) AS selected_options_titles,
            ARRAY_AGG(score)                     AS scores
     FROM poll_selections
            JOIN voters ON poll_selections.voter_id = voters.id
            JOIN poll_options ON poll_selections.poll_option_id = poll_options.id
     WHERE voters.poll_id = $1
-    GROUP BY voters.id, voters.poll_id, comments
+    GROUP BY voters.id, voters.poll_id, comment
     ORDER BY voter_id;
   `;
   return db.query(queryString, [id])
